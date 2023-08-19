355ed8d3e220174403db4c428f0f9c937c255584
Disha <dishavig1@gmail.com>
2023-08-19 22:41:18 +0530

    heap

---
+++ Heap/insert.cpp
@@ -0,0 +1,7 @@
+//max heap
+//insert an element in next free space
+//should form a complete binary tree
+//rearrange to form max heap
+//parent = i/2
+//i is where the element is being inserted
+//time=log(n)
