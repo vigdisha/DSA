921bcd4d496e38cd711be29cf5a11362c78fb5d7
Disha <dishavig1@gmail.com>
2023-08-22 23:35:24 +0530

    recursion

---
+++ Recursion/Tree.cpp
@@ -0,0 +1,14 @@
+// calling recursive function more than once
+#include <stdio.h>
+void fun(int n)
+{
+ if(n>0)
+ {
+ printf("%d ",n);
+ fun(n-1);
+ fun(n-1);
+ }
+}
+int main() {
+ fun(3);
+ return 0; }
