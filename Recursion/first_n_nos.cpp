fc0c2d116eb1faadd133bad419600e448383648d
Disha <dishavig1@gmail.com>
2023-08-26 01:26:40 +0530

    recursion

---
+++ Recursion/first_n_nos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void displayNumbers(int n) {
+    if (n >= 1) {
+        displayNumbers(n - 1); // Recursively call with n-1
+         cout << n << " ";
+       
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    displayNumbers(n);
+    return 0;
+}
---
+++ Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+   int x , i=0;
+//    cout << "Enter the number of terms of series : ";
+//    cin >> x;
+//    cout << "\nFibonnaci Series : ";
+//    cout<<fib(4)
