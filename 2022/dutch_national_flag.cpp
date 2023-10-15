ed462f9f3057a3b83c25fd386df96e8086a9394c
Disha <dishavig1@gmail.com>
2023-10-15 22:30:30 +0530

    add

--- 2022/dutch_national_flag.cpp
+++ 2022/dutch_national_flag.cpp
@@ -5,6 +5,35 @@
 // high = high...n-1
 
 //mid to high is unsorted
+void sortArray(vector<int>& arr, int n)
+{
+int low =0;
+int mid =0;
+int high = n-1;
+//only mid iterates
+while(mid<=high){
+    if(arr[mid] == 0)
+    {
+        swap(arr[mid], arr[low]);
+        mid++;
+        low++;
+    }
+    else if(arr[mid] == 2){
+        swap(arr[mid], arr[high]);
+        high--
+    }
+    else{
+        mid++;
+    }
+}
 
 
 
+
+
+
+
+
+
+
+}
