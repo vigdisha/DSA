414070452dbcf9ccbc1c44923bd03c6af6f5881e
Disha <dishavig1@gmail.com>
2023-10-19 16:34:31 +0530

    add

--- dp_av/0/knapsack.cpp
+++ dp_av/0/knapsack.cpp
@@ -1,33 +1,49 @@
-//Max profit 
-//given target weight
-//0/1 --> dp-->addition of fractional not allowed
-//dp_-->optimal
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int knapsack(vector<int> &weight,vector<int> &value, int knapsack_weight,int n){
-int max_profit=0;
-//BASE condition:samllest input/invalid situaution
-if(knapsack_weight>=0|| n>=0){
-    return 0;
-}
-
-//CHOICE diagram:
-//knapsack_weight>weight choosen
-if(wight[i]<=knapsack_weight){
-    knapsack(weight,value,knapsack_weight-weight[n-1],n-1);
+int knapsack(vector<int> &weight, vector<int> &value, int knapsack_weight, int n, vector<vector<int>> &dp) {
+    // BASE condition: smallest input/invalid situation
+    if (knapsack_weight = 0 || n = 0) {
+        return 0;
     }
-else{
-    return knapsack(weight, value, knapsack_weight, n-1)
+
+    // Memoization: base case
+    if (dp[n][knapsack_weight] != -1) {
+        return dp[n][knapsack_weight];
+    }
+
+    // CHOICE diagram:
+    // If the weight of the current item is less than or equal to knapsack_weight
+    if (weight[n - 1] <= knapsack_weight) {
+        // Calculate the maximum of two cases:
+        // 1. Include the current item
+        // 2. Exclude the current item
+        dp[n][knapsack_weight] = max(
+            value[n - 1] + knapsack(weight, value, knapsack_weight - weight[n - 1], n - 1, dp),
+            knapsack(weight, value, knapsack_weight, n - 1, dp)
+        );
+    } else {
+        // If the weight of the current item is greater than knapsack_weight, skip it
+        dp[n][knapsack_weight] = knapsack(weight, value, knapsack_weight, n - 1, dp);
+    }
+
+    return dp[n][knapsack_weight];
 }
 
-}
+int main() {
+    vector<int> weight = {2, 3, 4, 5};
+    vector<int> value = {3, 4, 5, 6};
+    int knapsack_weight = 5;
+    int n = weight.size();
 
+    // Initialize the DP table with -1 values
+    vector<vector<int>> dp(n + 1, vector<int>(knapsack_weight + 1, -1));
 
-int main()
-{
-  int weight[]={};
-    int value[]={};
-    int knapsack_weight=8;
+    int max_profit = knapsack(weight, value, knapsack_weight, n, dp);
+
+    cout << "Maximum Profit: " << max_profit << endl;
+
     return 0;
 }
+ 
---
+++ dp_av/0/knapsack_tabluation.cpp
@@ -0,0 +1,63 @@
+// Max profit
+// given target weight
+// 0/1 --> dp-->addition of fractional not allowed
+// dp_-->optimal
+#include <iostream>
+using namespace std;
+
+int knapsack(vector<int> &weight, vector<int> &value, int knapsack_weight, int n)
+{
+    int max_profit = 0;
+    // BASE condition:samllest input/invalid situaution
+    if (knapsack_weight = 0 || n == 0)
+    {
+        return 0;
+    }
+    // memoization:base case
+    if (dp[n][knapsack_weight] != -1)
+    {
+        return dp[n][knapsack_weight];
+    }
+
+    for (int i = 0; i < n + 1; i++)//-->ocnstitutes to n
+    {
+        for (int j = 0; j < knapsack_weight + 1; j++)//--->constitutes to knapsack_weight
+        {
+            if (i == 0 || j == 0)
+            {
+                return dp[i][j] = 0;
+            }
+            else if (weight[i-1] <= w)
+            {
+                // inlcude the value at the weight+
+                return dp[i][j] = max(value[i- 1] + dp[i- 1][j - w[i - 1]], dp[i - 1 ][w]);
+            }
+            else
+            {
+                return dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[n][knapsack_weight];
+}
+int main()
+{
+    vector<int> weight = {2, 3, 4, 5};
+    vector<int> value = {3, 4, 5, 6};
+    int knapsack_weight = 5;
+    int n = weight.size();
+
+    // Initialize the DP table with -1 values
+    vector<vector<int>> dp(n + 1, vector<int>(knapsack_weight + 1, -1));
+
+    int max_profit = knapsack(weight, value, knapsack_weight, n, dp);
+
+    cout << "Maximum Profit: " << max_profit << endl;
+
+    return 0;
+}
+
+// 1)recursive base condition-->intitilaization in tabulation
+// ie:if  (v==0||w==0)
+// return dp[m][n]=0;
+// 2)using loop :
