45bf17900417ec4cc3f6e1452a1a80e5ec87a0b2
Disha <dishavig1@gmail.com>
2023-10-25 00:03:44 +0530

    dp

--- dp/lcs/longest_comm_substring.cpp
+++ dp/lcs/longest_comm_substring.cpp
@@ -1,47 +1,60 @@
-//length of longest common substring
-// C++ implementation of the above approach
-#include <bits/stdc++.h>
+/* Dynamic Programming solution to 
+find length of the
+longest common substring */
+#include <iostream>
+#include <string.h>
 using namespace std;
 
-// Function to find the length of the
-// longest LCS
-int LCSubStr(string s, string t, int n, int m)
+/* Returns length of longest 
+common substring of X[0..m-1]
+and Y[0..n-1] */
+int LCSubStr(char* X, char* Y, int m, int n)
 {
+	// Create a table to store 
+	// lengths of longest
+	// common suffixes of substrings. 
+	// Note that LCSuff[i][j] contains 
+	// length of longest common suffix 
+	// of X[0..i-1] and Y[0..j-1].
 
-	// Create DP table
-	int dp[2][m + 1];
-	int res = 0;
-	
-	dp[0][0] = 0;
-	dp[1][0] = 0;
+	int LCSuff[m + 1][n + 1];
+	int result = 0; // To store length of the
+					// longest common substring
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (s[i - 1] == t[j - 1]) {
-				dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
-				if (dp[i % 2][j] > res)
-					res = dp[i % 2][j];
+	/* Following steps build LCSuff[m+1][n+1] in
+		bottom up fashion. */
+	for (int i = 0; i <= m; i++) 
+	{
+		for (int j = 0; j <= n; j++) 
+		{
+			// The first row and first column
+			// entries have no logical meaning,
+			// they are used only for simplicity
+			// of program
+			if (i == 0 || j == 0)
+				LCSuff[i][j] = 0;
+
+			else if (X[i - 1] == Y[j - 1]) {
+				LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+				result = max(result, LCSuff[i][j]);
 			}
 			else
-				dp[i % 2][j] = 0;
+				LCSuff[i][j] = 0;
 		}
 	}
-	return res;
+	return result;
 }
 
-// Driver Code
+// Driver code
 int main()
 {
-	string X = "OldSite:GeeksforGeeks.org";
-	string Y = "NewSite:GeeksQuiz.com";
+	char X[] = "OldSite:GeeksforGeeks.org";
+	char Y[] = "NewSite:GeeksQuiz.com";
 
-	int m = X.length();
-	int n = Y.length();
+	int m = strlen(X);
+	int n = strlen(Y);
 
-	cout << LCSubStr(X, Y, m, n);
-	return 0;
-	cout << "GFG!";
+	cout << "Length of Longest Common Substring is "
+		<< LCSubStr(X, Y, m, n);
 	return 0;
 }
-
-// This code is contributed by rajsanghavi9.
