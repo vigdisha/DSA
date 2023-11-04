c284c8800c963a5e5695a12d50006b28b17fa8c4
Disha <dishavig1@gmail.com>
2023-11-04 09:40:17 +0530

    dp

---
+++ Sort/size.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=4,b=6;
+    int sum=a+b;
+    // int *p = &num;
+//    (*p)++;
+int arr[10];
+    cout<<sizeof(arr);
+    return 0;
+}
